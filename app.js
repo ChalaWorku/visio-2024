62610753062786ec482a3d55a54ac5fa6d7ffd67
ChalaWorku <workuchala739@gmail.com>
2026-01-26 12:41:45 +0300

    hojii koo isaa jalqaba

---
+++ app.js
@@ -0,0 +1,1 @@
+const http = require('http'); const server = http.createServer((req, res) => { res.end('Baga nagaan gara marsariitii koo dhufte!'); }); server.listen(3000, () => { console.log('Serveriin kee http://localhost:3000 irratti hojjechaa jira'); });
